0791b55248b80b50733a3705f3d6363647e3b7a5
“[Sizzler_55]” <babarsmoke24@gmail.com>
2024-09-21 12:34:30 +0530

    Add tasks Implemented

--- TaskTracker.js
+++ TaskTracker.js
@@ -8,7 +8,35 @@ class TaskTracker {
   }
 
   async LoadTasks() {
-    const data = await fs.readFile(this.filename, "utf-8");
-    this.tasks = JSON.parse(data);
+    try {
+      const data = await fs.readFile(this.filename, "utf-8");
+      this.tasks = JSON.parse(data);
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        console.log(
+          "No Tasks found in the file , Starting with an empty Task List"
+        );
+        this.tasks = [];
+      }
+    }
   }
+
+  async addTask(title, description, dueDate) {
+    const task = {
+      id: this.tasks.length + 1,
+      title,
+      description,
+      dueDate,
+      completed: false,
+      createdAt: new Date().toISOString(),
+    };
+    this.tasks.push(task);
+    await this.SaveTask();
+    return task;
+  }
+  async removeTask() {}
+
+  async MarkTaskCompleted() {}
+  async ListTask() {}
+  async SaveTask() {}
 }
