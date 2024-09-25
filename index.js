b53a0895128a9e6f5f6321b9e3142c0eac9f3902
“[Sizzler_55]” <babarsmoke24@gmail.com>
2024-09-25 15:47:47 +0530

    Colors Implemented

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 import TaskTracker from "./TaskTracker.js";
 import inquirer from "inquirer";
+import chalk from "chalk";
 const taskTracker = new TaskTracker();
 await taskTracker.LoadTasks();
 
 async function MainMenu() {
-  console.log("Welcome To CLI for TaskTracking !!");
+  console.log(chalk.green("Welcome To CLI for TaskTracking !!"));
 
   const { choice } = await inquirer.prompt([
     {
       type: "list",
       name: "choice",
-      message: "What would you like to do ? (Use arrow Keys )",
+      message: chalk.blue("What would you like to do ? (Use arrow Keys )"),
       choices: [
-        "Add a Task",
-        "List all Tasks",
-        "Save Tasks",
-        "Remove Tasks",
-        "Mark Tasks Completed",
-        "Exit",
+        { name: chalk.bold.magenta("Add a Task"), value: "Add a Task" },
+        { name: chalk.bold.magenta("List all Tasks"), value: "List all Tasks" },
+        { name: chalk.bold.magenta("Save Tasks"), value: "Save Tasks" },
+        { name: chalk.bold.magenta("Remove Tasks"), value: "Remove Tasks" },
+
+        { name: chalk.red("Exit"), value: "Exit" },
       ],
     },
   ]);
@@ -38,7 +39,7 @@ async function MainMenu() {
       await MarkTaskCompleted();
       break;
     case "Exit":
-      console.log("Thank you for using CLI !!");
+      console.log("Thank you for using CLI");
       process.exit(0);
   }
   //TODO : 1) Adding Colors , 2 ) After Exiting : Thank you For using CLI Inteface  3) Do you want to exit the Cli or want to write another task?
@@ -48,21 +49,21 @@ async function MainMenu() {
       {
         type: "input",
         name: "title",
-        message: "Enter task title",
+        message: chalk.magenta("Enter task title"),
       },
       {
         type: "input",
         name: "description",
-        message: "Enter task description",
+        message: chalk.magenta("Enter task description"),
       },
       {
         type: "input",
         name: "dueDate",
-        message: "Enter task DueDate",
+        message: chalk.magenta("Enter task DueDate"),
       },
     ]);
     taskTracker.addTask(title, description, dueDate);
-    console.log("Task added Successfully! ");
+    console.log(chalk.green("Task added Successfully! "));
   }
 }
 
@@ -80,43 +81,43 @@ async function SaveTask() {
   // }
   try {
     await taskTracker.SaveTask();
-    console.log("Task saved Successfully");
+    console.log(chalk.green("Task saved Successfully! "));
   } catch (error) {
-    console.log("Error found ", error);
+    console.log(chalk.red("Error found ", error));
   }
 }
 
 async function removeTask() {
   const tasks = taskTracker.removeTask();
   if (tasks.length === 0) {
-    console.log("No Tasks to remove  ");
+    console.log(chalk.yellow("No Tasks to remove "));
     return;
   }
   const { taskId } = await inquirer.prompt([
     {
       type: "input",
       name: "taskId",
-      message: "Enter TaskId",
+      message: chalk.magenta("Enter TaskId"),
     },
   ]);
   const removed = await taskTracker.removeTask(taskId);
   if (removed) {
-    console.log("Task removed successfully");
+    console.log(chalk.green("Task removed Successfully! "));
   } else {
-    console.log("Task not found ");
+    console.log(chalk.yellow("Task not found "));
   }
 }
 async function MarkTaskCompleted() {
   const tasks = taskTracker.ListTask();
   if (tasks.length === 0) {
-    console.log("No Tasks can be found ");
+    console.log(chalk.yellow("No Tasks can be found "));
     return;
   }
   const { taskId } = await inquirer.prompt([
     {
       type: "number",
       name: "taskId",
-      message: "Enter taskId",
+      message: chalk.magenta("Enter taskId"),
     },
   ]);
   console.log({ taskId });
@@ -126,9 +127,9 @@ async function MarkTaskCompleted() {
     return;
   } else {
     if (MarkCompleted) {
-      console.log("Task Completed");
+      console.log(chalk.green("Task Completed"));
     } else {
-      console.log("Task is not Completed yet!");
+      console.log(chalk.yellow("Task is not Completed yet!"));
     }
   }
 }
